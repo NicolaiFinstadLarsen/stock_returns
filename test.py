25c838a3482637ae9acaf8d511b2c9512fc0f8fc
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-11-08 21:46:59 +0100

    Still OpenSSL problems

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ import datetime as dt
 # from pandas_datareader import data as pdr
 # Can be used for collecting other data. 
 
-tickers = ["AAPL", "QQQ", "MSFT", "TSLA", "SPY"]
+tickers = ["SPY"]
 
 EndDate = dt.datetime.now()
 StartDate = dt.datetime(2024, 1, 1)
 
-df = yf.download(tickers, start = StartDate, end = EndDate)
+df = yf.download(tickers, start = StartDate, end = EndDate, use_threads=False)
 print(df.head())
 
 # Det funker ikke bedre nå.
